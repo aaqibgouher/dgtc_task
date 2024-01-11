ef3ca38fed7de6ada2e52589d8270bd714cd3661
aaqibgouher <aaqibgouher@gmail.com>
2024-01-11 12:42:37 +0530

    changed the swagger path

--- server/swagger_config.js
+++ server/swagger_config.js
@@ -1,7 +1,10 @@
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("js-yaml");
 const fs = require("fs");
-const swaggerJSDocs = YAML.load(fs.readFileSync("./swagger.yaml", "utf8"));
+// const swaggerJSDocs = YAML.load(fs.readFileSync("./swagger.yaml", "utf8"));
+const swaggerJSDocs = YAML.load(
+  fs.readFileSync(__dirname + "/swagger.yaml", "utf8")
+);
 
 module.exports = {
   swaggerServe: swaggerUI.serve,
